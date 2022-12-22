462d6c81e5503382dc0b50d6367a60c8f38a3703
Uladzislau Roslikau <uladzislau.rosgro@gmail.com>
2022-12-22 18:58:20 +0300

    feat: add new test

--- src/test/java/com/rosgro/test/MainPageTest.java
+++ src/test/java/com/rosgro/test/MainPageTest.java
@@ -12,11 +12,14 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
 
+import com.rosgro.page.CartPage;
 import com.rosgro.page.MainPage;
 import com.rosgro.model.Product;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.rosgro.page.ProductPage;
 import org.testng.annotations.Test;
 
 public class MainPageTest extends AbstractTest {
@@ -55,5 +58,16 @@ public class MainPageTest extends AbstractTest {
     String LinkAfterCountryChange = new MainPage(driver,MAIN_PAGE).changeCountry();
     assertThat(LinkAfterCountryChange,equalTo(UnitedKingdomFilaLink));
   }
+
+  @Test
+  public void addManyToCart() {
+    final String SECOND_PRODUCT_PAGE = "https://www.fila.com/mens-grant-hill-2/1BM01789.html?dwvar_1BM01789_size=11&dwvar_1BM01789_color=132&fromList=Category%3A%20Sneakers%20%26%20Lifestyle&gridposition=20&cgid=men-sneakers";
+    Product productPageSneaker = new ProductPage(driver, PRODUCT_PAGE).addToCart().createProduct();
+    Product secondSneaker = new ProductPage(driver, SECOND_PRODUCT_PAGE).addToCart().createProduct();
+    Product cartPageSneaker = new CartPage(driver, CART_PAGE).findProduct().get(0);
+    assertThat(productPageSneaker.getPrice() + secondSneaker.getPrice(), equalTo(cartPageSneaker.getPrice()));
+  }
+
+
 
 }
--- src/test/java/com/rosgro/test/ProductPageTest.java
+++ src/test/java/com/rosgro/test/ProductPageTest.java
@@ -8,6 +8,7 @@ import static org.hamcrest.Matchers.equalTo;
 import com.rosgro.model.Product;
 import com.rosgro.page.CartPage;
 import com.rosgro.page.ProductPage;
+import net.bytebuddy.build.Plugin;
 import org.testng.annotations.Test;
 
 public class ProductPageTest extends AbstractTest {
@@ -18,16 +19,6 @@ public class ProductPageTest extends AbstractTest {
         Product cartPageSneaker = new CartPage(driver, CART_PAGE).findProduct().get(0);
         assertThat(productPageSneaker, equalTo(cartPageSneaker));
     }
-
-    @Test
-    public void addManyToCart() {
-        final String SECOND_PRODUCT_PAGE = "https://www.fila.com/mens-grant-hill-2/1BM01789.html?dwvar_1BM01789_size=11&dwvar_1BM01789_color=132&fromList=Category%3A%20Sneakers%20%26%20Lifestyle&gridposition=20&cgid=men-sneakers";
-        Product productPageSneaker = new ProductPage(driver, PRODUCT_PAGE).addToCart().createProduct();
-        Product secondSneaker = new ProductPage(driver, SECOND_PRODUCT_PAGE).addToCart().createProduct();
-        Product cartPageSneaker = new CartPage(driver, CART_PAGE).findProduct().get(0);
-        assertThat(productPageSneaker.getPrice() + secondSneaker.getPrice(), equalTo(cartPageSneaker.getPrice()));
-    }
-
     @Test
     public void removeFromCart() {
         final String emptyBagText = "Your Bag (0)";
